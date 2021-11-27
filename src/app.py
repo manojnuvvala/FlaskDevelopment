c20d33826fe2f1c4e0577d83a662dba868e63878
saikiranreddyenugala <77818423+saikiranreddyenugala@users.noreply.github.com>
2021-11-27 13:11:39 -0600

    connecting postgresql database

--- src/app.py
+++ src/app.py
@@ -15,8 +15,13 @@ csrf_protection = CSRFProtect()
 def create_app():
 
     app = Flask(__name__)
-    app.config.from_pyfile("config.py")
-
+    #app.config.from_pyfile("config.py")
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/flaskjoke'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_POOL_SIZE "] = 20
+    app.config["SQLALCHEMY_MAX_OVERFLOW"] = 100
+    app.secret_key = 'secret string'
+    db = SQLAlchemy(app)
     from .views import app as main_blueprint
 
     db.init_app(app)
