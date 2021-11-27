15d49557aaf4bc743d021bc7aac851a8da8e83b8
manojnuvvala <77800099+manojnuvvala@users.noreply.github.com>
2021-11-28 02:52:16 +0500

    creation of tables

--- db_create.py
+++ db_create.py
@@ -1,2 +1,70 @@
 from src.app import db
+import random
+
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from src.app import login_manager
+
+
+class User(UserMixin, db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password_hash = generate_password_hash(password)
+
+    @property
+    def password(self):
+        raise AttributeError("Password cannot be read")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "<User %r>" % self.email
+
+
+class Joke(db.Model):
+
+    __tablename__ = "jokes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    joke = db.Column(db.String(120), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def __init__(self, joke, user_id=1):
+        self.joke = joke
+        self.user_id = user_id
+
+    def save_joke(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def get_random_joke():
+        return random.choice(Joke.query.all())
+
+    def __repr__(self):
+        return f"<Joke {self.id}, {self.joke}, {self.user_id}>"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    from .models import User
+
+    return User.query.get(user_id)
+
 db.create_all()
