ddf2b0aee8f0cd676641456859b9a1b07755da54
manojnuvvala <77800099+manojnuvvala@users.noreply.github.com>
2021-11-29 03:40:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask
+from src.app import create_app
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+if __name__ == "__main__":
+    app = create_app()
+    app.run()
