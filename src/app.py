c61444978c4a8aa23bfd001df9707e4d138473b4
saikiranreddyenugala <77818423+saikiranreddyenugala@users.noreply.github.com>
2021-11-29 13:23:24 -0600

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, app
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
@@ -8,29 +8,21 @@ login_manager = LoginManager()
 login_manager.session_protection = "strong"
 login_manager.login_view = "auth.login"
 
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
-if uri and uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_POOL_SIZE "] = 20
-app.config["SQLALCHEMY_MAX_OVERFLOW"] = 100
-app.secret_key = 'secret string'
-db = SQLAlchemy(app)
-db.create_all()
+
+db = SQLAlchemy()
+
 csrf_protection = CSRFProtect()
 
 
 def create_app():
 
     app = Flask(__name__)
-    #app.config.from_pyfile("config.py")
-    app.config['SQLALCHEMY_DATABASE_URI'] = uri
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SQLALCHEMY_POOL_SIZE "] = 20
-    app.config["SQLALCHEMY_MAX_OVERFLOW"] = 100
-    app.secret_key = 'secret string'
+    app.config.from_pyfile("config.py")
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/flaskjoke'
+   # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    #app.config["SQLALCHEMY_POOL_SIZE "] = 20
+    #app.config["SQLALCHEMY_MAX_OVERFLOW"] = 100
+    #app.secret_key = 'secret string'
     db = SQLAlchemy(app)
     from .views import app as main_blueprint
 
