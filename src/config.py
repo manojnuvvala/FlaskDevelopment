d293f01a461c2264a9508007a3f5f996322d55eb
saikiranreddyenugala <77818423+saikiranreddyenugala@users.noreply.github.com>
2021-12-01 13:49:47 -0600

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,4 +2,20 @@ APP_PORT = 7000
 
 SECRET_KEY = "secret"
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost/flaskjoke'
+#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost/flaskjoke'
+SQLALCHEMY_DATABASE_URI ='postgresql://dydskibgmtjyvu:36d4a41f2e4e6aa6580f08b38b854be6a2b49d08e5575ab7d20c0df3d8c38c56@ec2-107-23-41-227.compute-1.amazonaws.com:5432/dcnop13pt8cr8m'
+
+#import flask_login
+
+
+#app = flask_login(__name__)
+
+#POSRGRES = {
+    #'user' : 'dydskibgmtjyvu',
+    #'pw':'36d4a41f2e4e6aa6580f08b38b854be6a2b49d08e5575ab7d20c0df3d8c38c56',
+    #'db' : 'dcnop13pt8cr8m',
+    #'host' : 'ec2-107-23-41-227.compute-1.amazonaws.com',
+    #'port' : '5432'
+#}
+
+
